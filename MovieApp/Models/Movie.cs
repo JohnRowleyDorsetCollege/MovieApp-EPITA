07fe8c57b5aa24d284ed943c4960c915c54f8b7f
dctutor <dctutor@DESKTOP-O8SCFH1>
2024-01-16 10:37:57 +0000

    Add project files.

---
+++ MovieApp/Models/Movie.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Security.Cryptography;
+// https://pastebin.com/3gKF0CSE - initial migration
+namespace MovieApp.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; } // Required for database - primary key
+        public string Title { get; set; } = string.Empty;
+
+        [DataType(DataType.Date)] // annotation
+        public DateTime ReleaseDate { get; set; }
+        public string?  Genre  { get; set;}
+        public decimal Price { get; set; }
+    }
+}
