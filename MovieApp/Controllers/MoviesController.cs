6ca30cfda90bc83e86a2f99a463692e19240465b
johnrowley <john.rowley@dorset.ie>
2024-01-16 11:15:14 +0000

    Added search string

--- MovieApp/Controllers/MoviesController.cs
+++ MovieApp/Controllers/MoviesController.cs
@@ -20,9 +20,23 @@ namespace MovieApp.Controllers
         }
 
         // GET: Movies
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index(string searchString)
         {
-            return View(await _context.Movie.ToListAsync());
+            if (_context.Movie is null)
+            {
+                return Problem("Entity context is null");
+            }
+
+            var movies = from m in _context.Movie select m;
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+
+                movies = movies.Where(s => s.Title.Contains(searchString));
+            }
+
+
+            return View(await movies.ToListAsync());
         }
 
         // GET: Movies/Details/5
